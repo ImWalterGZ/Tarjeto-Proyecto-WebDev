414c987e7166fdef02a19d0ed6a38cf2e89df017
Aldo Barrera <aldobg953@gmail.com>
2024-11-21 23:53:14 -0600

    Actualización de textos y estilos en las pantallas de inicio de sesión y registro

--- src/app/components/Registro.js
+++ src/app/components/Registro.js
@@ -111,13 +111,12 @@ export default function Registro({ setRegistro }){
                         ) : null}
                     </div>
                     <div><button type="submit" onClick={registerUser} className='mb-4 py-2 px-5 me-2 text-m w-full text-white font-bold focus:outline-none 
-              bg-red-500 rounded-full hover:bg-red-600 focus:z-10 focus:ring-4 
-              focus:ring-gray-100'>¡Crear cuenta!</button></div>
-                    <button type="button"  className="mb-4 w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-full text-sm pl-14 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2">
+              bg-red-500 rounded-full hover:bg-red-600 focus:z-10'>¡Crear cuenta!</button></div>
+                    <button type="button"  className="mb-4 w-full text-white  bg-[#434343] hover:bg-[#434343]/90 focus:outline-nonefont-medium rounded-full text-sm pl-[4.5rem] pr-0 py-2.5 text-center inline-flex items-center me-2">
                     <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
                     <path d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"/>
                     </svg>
-                    Sign in with Google
+                    Registrarse con Google
                     </button>
                     <div className='text-xs text-[#434343]'>¿Ya tienes una cuenta? ¡Inicia sesión <a onClick={setRegistro} className='text-xs text-[#f4262f] cursor-pointer hover:font-bold'>aquí</a>!</div>
                 </Form>
